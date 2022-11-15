acda5b05ae991fb456987448b1b59ef2d3fb19de
VincenzoZ24 <zitov287@gmail.com>
2022-11-15 15:06:14 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+new Vue ({
+    el: "#root",
+    data:{
+        todo: [
+            {
+                text: "fare gli esercizi",
+                done: true
+            },
+            {
+                text: "andare in palestra",
+                done: true
+            }
+        ]
+    }
+})
